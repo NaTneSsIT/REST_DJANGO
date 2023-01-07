6d343c10d7a94e53178430dbafef9fdf1e657816
NaTneSsIT <bestnnt2001@gmail.com>
2023-01-07 10:33:28 +0700

    update

--- posts/views.py
+++ posts/views.py
@@ -11,29 +11,6 @@ from rest_framework import generics
 # Create your views here.
 
 
-# class GetAllInfoAPI(APIView):
-#     def get(self, request):
-#         list_info = posts.objects.all()
-#         mydata = GetAllInfo(list_info, many=True)
-#         return Response(data=mydata.data, status=status.HTTP_200_OK)
-#
-#     def post(self, request):
-#         mydata = PostSerializer(request.data)
-#         if not mydata.is_valid():
-#             return Response('Sai du lieu roi!',status=status.HTTP_400_BAD_REQUEST)
-#         title=mydata.data['title1']
-#         desciption=mydata.data['desciption1']
-#         posts.objects.create(title=title,desciption=desciption)
-#         return Response(data=mydata.data, status=status.HTTP_200_OK)
-
-# class ItemList(generics.ListCreateAPIView):
-#     serializer_class=GetAllInfo
-#
-#     def get_queryset(self):
-#         queryset=posts.objects.all()
-#         location =self.request.query_params.get('location')
-#         if location is not None:
-#             queryset=queryset.filter()
 @api_view(['GET'])
 def taskList(request):
     tasks = posts.objects.all().order_by('-id')

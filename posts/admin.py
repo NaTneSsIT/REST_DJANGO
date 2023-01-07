03cd99518b76ccc4b85711f11a22b15e28ac54f8
NaTneSsIT <bestnnt2001@gmail.com>
2023-01-07 10:26:35 +0700

    update filter admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import posts
 # Register your models here.
-admin.site.register(posts)
+
+class postAdmin(admin.ModelAdmin):
+    list_display=("id","title","desciption","create_at","update_at")
+    search_fields=['title']
+    list_filter=("id","title","desciption")
+admin.site.register(posts,postAdmin)
